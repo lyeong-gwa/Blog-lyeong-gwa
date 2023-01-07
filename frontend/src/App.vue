7244462e002f56662abfa43bd3571d68e7e4900c
박정희 <wjdgmll77@naver.com>
2023-01-07 20:09:47 +0900

    Add: footer, 구글 애널리틱스 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,27 @@
     <Drawer></Drawer>
     <AppBar></AppBar>
     <v-main>
-      <v-card class="mx-auto" max-width="1000" style="margin-top: 30px">
+      <v-card class="mx-auto" max-width="1300" style="margin-top: 30px">
         <router-view />
       </v-card>
-      
     </v-main>
+    <v-footer dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-text class="py-2 white--text text-center">
+          {{ new Date().getFullYear() }} —
+          <strong>
+            © www.coregemstone.com All Rights Reserved. www.coregemstone.com is
+            not associated with NEXON Korea.</strong
+          >
+          <v-spacer></v-spacer>
+          <v-btn v-for="item in icons" :key="item" class="mx-4" dark icon :href="item.link">
+            <v-icon size="24px">
+              {{ item.icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
@@ -22,5 +38,8 @@ export default {
     AppBar,
     Drawer,
   },
+  data: () => ({
+    icons: [{icon:"mdi-github",link:"https://github.com/lyeong-gwa"}],
+  }),
 };
 </script>
